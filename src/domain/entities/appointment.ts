73f8a24590375aa0f64cb04e2741b8e767be13a4
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-06-10 13:58:24 -0300

    feat: add customer to appointments

--- src/domain/entities/appointment.ts
+++ src/domain/entities/appointment.ts
@@ -1,6 +1,7 @@
 interface AppointmentProps {
   startsAt: Date;
   endsAt: Date;
+  customer: string;
 }
 
 export class Appointment {
@@ -12,5 +13,9 @@ export class Appointment {
 
   get endsAt() {
     return this.props.endsAt;
+  }
+
+  get customer() {
+    return this.props.customer;
   }
 }
