5b88e9f11e24bd6b345ebc8a56e730a8352945e9
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-07-02 11:10:07 -0300

    feat: add name to barbers

--- src/domain/entities/barber.spec.ts
+++ src/domain/entities/barber.spec.ts
@@ -1,4 +1,10 @@
-class Barber {}
+interface BarberProps {
+  name: string;
+}
+
+class Barber {
+  constructor(private props: BarberProps) {}
+}
 
 describe('Barber entity', () => {
   it('should be able to create a barber', () => {
