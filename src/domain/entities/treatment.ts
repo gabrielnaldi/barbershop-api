094c4e28ec95c20534d050d0f41b45f79d718bb0
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-07-02 14:10:35 -0300

    feat: add price to treatment

--- src/domain/entities/treatment.ts
+++ src/domain/entities/treatment.ts
@@ -1,7 +1,11 @@
 export class Treatment {
-  constructor(private props: { name: string }) {}
+  constructor(private props: { name: string; price: number }) {}
 
   get name() {
     return this.props.name;
+  }
+
+  get price() {
+    return this.props.price;
   }
 }
