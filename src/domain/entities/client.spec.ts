0441348dadbc5e7a0b80dd4fba15fcd5a871103a
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-07-02 10:58:38 -0300

    test: ensure clients can be created with emails

--- src/domain/entities/client.spec.ts
+++ src/domain/entities/client.spec.ts
@@ -14,5 +14,20 @@ describe('Client entitie', () => {
     expect(client.name).toBe(clientData.name);
     expect(client.birthDate).toBe(clientData.birthDate);
     expect(client.phone).toBe(clientData.phone);
+    expect(client.email).toBeUndefined();
+  });
+
+  it('should be able to create clients with emails', () => {
+    const clientData = {
+      name: 'Lorem ipsum',
+      birthDate: new Date(),
+      phone: '+5512912345678',
+      email: 'lorem@gmail.com',
+    };
+
+    const client = new Client(clientData);
+
+    expect(client).toHaveProperty('props.email');
+    expect(client.email).toBe(clientData.email);
   });
 });
