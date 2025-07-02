779ed2c9d5ebb49d00ba1ad40085931ace43051d
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-07-02 14:16:20 -0300

    test: ensure it can create a treatment with price and name

--- src/domain/entities/treatment.spec.ts
+++ src/domain/entities/treatment.spec.ts
@@ -1,14 +1,16 @@
 import { Treatment } from './treatment';
 
 describe('Product entity', () => {
-  it('should create a Treatment with name', () => {
+  it('should be able to create a Treatment with name and price', () => {
     const treatmentData = {
       name: 'Barba',
+      price: 5000,
     };
 
     const treatment = new Treatment(treatmentData);
 
     expect(treatment).toBeInstanceOf(Treatment);
     expect(treatment.name).toBe(treatmentData.name);
+    expect(treatment.price).toBe(treatmentData.price);
   });
 });
