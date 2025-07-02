4d287cdb7f7997f8c86aa5e7adb9980319b43de2
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-07-02 10:53:24 -0300

    feat: add properties to clients

--- src/domain/entities/client.ts
+++ src/domain/entities/client.ts
@@ -1,1 +1,21 @@
-export class Client {}
+interface ClientProps {
+  name: string;
+  phone: string;
+  birthDate: Date;
+}
+
+export class Client {
+  constructor(private props: ClientProps) {}
+
+  get name() {
+    return this.props.name;
+  }
+
+  get birthDate() {
+    return this.props.birthDate;
+  }
+
+  get phone() {
+    return this.props.phone;
+  }
+}
