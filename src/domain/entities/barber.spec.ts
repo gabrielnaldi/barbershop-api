b0b921824dd97227d7c95aae1d254e7488e62b1c
Gabriel N <gabrielnaldi.castro@gmail.com>
2025-07-02 11:16:38 -0300

    feat: add specialties to barbers

--- src/domain/entities/barber.spec.ts
+++ src/domain/entities/barber.spec.ts
@@ -1,19 +1,26 @@
 interface BarberProps {
   name: string;
+  specialties: Array<string>;
 }
 
 class Barber {
   constructor(private props: BarberProps) {}
+
+  get name() {
+    return this.props.name;
+  }
 }
 
 describe('Barber entity', () => {
-  it('should make sure a barber has name ', () => {
+  it('should make sure a barber has name', () => {
     const barberData: BarberProps = {
       name: 'Augusto',
+      specialties: [],
     };
 
     const barber = new Barber(barberData);
 
     expect(barber).toBeInstanceOf(Barber);
+    expect(barber.name).toBe(barberData.name);
   });
 });
